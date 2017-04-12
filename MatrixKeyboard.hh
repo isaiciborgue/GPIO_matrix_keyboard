5f8211a71aca7afebddaca08b93520216cad9463
Isai Alcântara <isaiisswave@gmail.com>
2017-04-12 18:09:20 -0300

    Full reworked class

--- MatrixKeyboard.hh
+++ MatrixKeyboard.hh
@@ -1,11 +1,3 @@
-// MatrixKeyboard.hh
-/*
- *	A class that creates a simple matrix keyboard 
- * 	using the wiringPi library and the X11 Xtest 
- * 	extension.
- */
-
-
 #ifndef _MATRIX_KEYBOARD_HH
 #define _MATRIX_KEYBOARD_HH
 
@@ -17,41 +9,33 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <vector>
 
 using namespace std;
 using namespace std::chrono;
 using namespace std::this_thread;
 
-typedef unsigned int uInt;
-struct Key
-{
-	uInt rowPin;
-	uInt colPin;
-	KeySym keysym;
-	uInt lastState;
-};
-
-static const bool KEY_DOWN = true;
-static const bool KEY_UP = false;
-static const uInt nRows = 1;
-static const uInt nCols = 2;
+typedef unsigned int uInt; 
 
 class MatrixKeyboard
 {
 private:
-
 	Display * myXDisplay;
-	Key keyboard[nRows][nCols];
+	vector<uInt> rows;
+	vector<uInt> cols;
+	vector<KeySym> keys;
+	vector<bool> lastStates;
+	uInt delay;
 
 public:
-	MatrixKeyboard();
+	MatrixKeyboard(vector<uInt> rows, vector<uInt> cols, vector<KeySym> keys);
+	void run();
 
 private:
 	void pressKey(KeySym keysym);
 	void releaseKey(KeySym keysym);
 
-public:
-	void run();
+
 };
 
 #endif // ! _MATRIX_KEYBOARD_HH
